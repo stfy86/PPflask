ce9b020e82161b5a1469b19a8691c525fe9d648c
silvana <silvana@silvanaHP>
2013-05-14 14:07:27 -0400

    14/may: manage para la it4

--- pruebita/src/manage.py
+++ pruebita/src/manage.py
@@ -19,58 +19,41 @@ def initdb():
     createRol()
     createProject()
     createTipoDeAtrib()
-    createFase()
-
  
 @manager.command
 def createUser():
     """ Crea usuarios """ 
     from models import User
     from ctrl.mgrUser import MgrUser
-    u = User("admin","admin","administrador","administrador","admin@gmail.com",1234,"usuario administrador")
-    MgrUser().guardar(u)
-    u=User("stfy","stfy","estefanis","zamora","stfy@gmail.com",1111,"usuario nuevo")
-    MgrUser().guardar(u)
-    u=User("vavi","vavi","victor","vera","vavi@gmail.com",2222,"usuario nuevo")
-    MgrUser().guardar(u)
-    u=User("lory","lory","lorelay","ortiz","lory@gmail.com",3333,"usuario nuevo")
-    MgrUser().guardar(u)
-    u=User("guille","guille","guillermo","gonzalez","guille@gmail.com",4444,"usuario nuevo")
-    MgrUser().guardar(u)
-    #MgrUser().borrar("guille")
+    usuarios = [ User("admin","admin","administrador","administrador","admin@gmail.com",1234,"usuario administrador"),
+                User("stfy","stfy","estefanis","zamora","stfy@gmail.com",1111,"usuario nuevo"),
+                User("vavi","vavi","victor","vera","vavi@gmail.com",2222,"usuario nuevo"),
+                User("lory","lory","lorelay","ortiz","lory@gmail.com",3333,"usuario nuevo"),
+                User("guille","guille","guillermo","gonzalez","guille@gmail.com",4444,"usuario nuevo")]
+    MgrUser().guardar(usuarios)
        
 @manager.command
 def createRol():
     """ 
     Crea Roles de Sistema Pre establecidos 
     1. Administrador: -> Permite el acceso al Modulo de Administracion
-        - administra usuarios
-        - crea proyecto
-        - elimina proyecto
-        - asigna lider a proyecto
-        - administra tipo de atributo
     2. Desarrollador: -> Permite el acceso al Modulo de Desarrollo
-        - administra item
-        - administra reportes
     3. Lider de Proyecto: -> Permite el acceso al Modulo de Gestion de Cambio
-        - administra proyecto
-        - administra fase
-        - administra tipo de item
-        - administra roles 
-        - administra LB
-        - calculo de costo
-        - calculo de impacto
     """
     from models import Rol
     from ctrl.mgrRol import MgrRol
-    r=Rol("Administrador","permite el acceso al modulo de administracion","all project")
-    MgrRol().guardar(r)
-    r=Rol("Desarrollador","permite el acceso al modulo de desarrollo","all project")
-    MgrRol().guardar(r)
-    r=Rol("LiderDeProyecto","permite el acceso al modulo de gestion","all project")
-    MgrRol().guardar(r)
-    #MgrRol().borrar("LiderDeProyecto")
-
+    from models import Permiso
+    from ctrl.mgrPermiso import MgrPermiso
+    pAdmin = [Permiso("ModuloAdministracion", "permite el acceso al modulo de administracion")]
+    rAdmin = Rol("Administrador", "permite el acceso al modulo de administracion", "all project", pAdmin)
+    MgrRol().guardar(rAdmin)
+    pDesar = [Permiso("ModuloDesarrollo", "permite el acceso al modulo de desarrollo")]
+    rDesar = Rol("Desarrollador", "permite el acceso al modulo de desarrollo", "all project", pDesar)
+    MgrRol().guardar(rDesar)
+    pLider = [Permiso("ModuloGestion", "permite el acceso al modulo de gestion")]
+    rLider=Rol("LiderDeProyecto","permite el acceso al modulo de gestion","all project", pLider)
+    MgrRol().guardar(rLider)
+    
 @manager.command
 def createPermiso():
     """
@@ -82,79 +65,45 @@ def createPermiso():
     """
     from models import Permiso
     from ctrl.mgrPermiso import MgrPermiso
-    # Los permisos a Nivel de Sistema son
-    p=Permiso("CrearProyecto","Permite crear un proyecto en el sistema")
-    MgrPermiso().guardar(p)
-    p=Permiso("CrearUsuario","Permite crear un usuario dentro de un proyecto")
-    MgrPermiso().guardar(p)
-    p=Permiso("CambiarEstadoUsuario","Permite cambiar el estado de un usuario dentro del proyecto")
-    MgrPermiso().guardar(p)
-    p=Permiso("AdministrarTipoDeAtributo","Permite administrar un tipo de atributo")
-    MgrPermiso().guardar(p)
-    # Los Permisos a Nivel de Proyecto son
-    p=Permiso("CrearRol","Permite crear un rol en el sistema")
-    MgrPermiso().guardar(p)
-    p=Permiso("AsignarRolAUsuario","Permite asignar Rol a Usuario")
-    MgrPermiso().guardar(p)
-    p=Permiso("AdministrarFase","Permite administrar fase en un proyecto")
-    MgrPermiso().guardar(p)
-    p=Permiso("ConsultaProyecto","Permite realizar consultas en un proyecto")
-    MgrPermiso().guardar(p)
-    # Los Permisos a Nivel de Fase son
-    p=Permiso("AdministrarLineaBase","Permite administrar linea base en un proyecto")
-    MgrPermiso().guardar(p)
-    p=Permiso("AdministrarTiposDeItem","Permite administrar tipos de item en un proyecto")
-    MgrPermiso().guardar(p)
-    # Los Permisos a Nivel de Item son
-    p=Permiso("AdministrarItem","Permite administrar item en un proyecto")
-    MgrPermiso().guardar(p)
-    p=Permiso("AdministrarCambio","Permite administrar cambio en un item de un proyecto")
-    MgrPermiso().guardar(p)
-    p=Permiso("AprobacionItem","Permite administrar la aprobacion de item en un proyecto")
-    MgrPermiso().guardar(p)
-    p=Permiso("DesaprobacionItem","Permite administrar la desaprobacion de item en un proyecto")
-    MgrPermiso().guardar(p)
-    p=Permiso("ConsultaItem","Permite consulta de item en un proyecto")
-    MgrPermiso().guardar(p)
-    #MgrPermiso().borrar("ConsultaItem")
- 
+    permisos = [ # Los permisos a Nivel de Sistema son
+                Permiso("CrearProyecto","Permite crear un proyecto en el sistema"),
+                Permiso("CrearUsuario","Permite crear un usuario dentro de un proyecto"),
+                Permiso("CambiarEstadoUsuario","Permite cambiar el estado de un usuario dentro del proyecto"),
+                Permiso("AdministrarTipoDeAtributo","Permite administrar un tipo de atributo"),
+                # Los Permisos a Nivel de Proyecto son
+                Permiso("CrearRol","Permite crear un rol en el sistema"),
+                Permiso("AsignarRolAUsuario","Permite asignar Rol a Usuario"),
+                Permiso("AdministrarFase","Permite administrar fase en un proyecto"),
+                Permiso("ConsultaProyecto","Permite realizar consultas en un proyecto"),
+                # Los Permisos a Nivel de Fase son
+                Permiso("AdministrarLineaBase","Permite administrar linea base en un proyecto"),
+                Permiso("AdministrarTiposDeItem","Permite administrar tipos de item en un proyecto"),
+                # Los Permisos a Nivel de Item son
+                Permiso("AdministrarItem","Permite administrar item en un proyecto"),
+                Permiso("AdministrarCambio","Permite administrar cambio en un item de un proyecto"),
+                Permiso("AprobacionItem","Permite administrar la aprobacion de item en un proyecto"),
+                Permiso("DesaprobacionItem","Permite administrar la desaprobacion de item en un proyecto"),
+                Permiso("ConsultaItem","Permite consulta de item en un proyecto")]
+    MgrPermiso().guardar(permisos)
+
 @manager.command
 def createProject():
     """ Crea proyectos por default """
     from models import Proyecto
     from ctrl.mgrProject import MgrProject
-    p=Proyecto("proyecto1","sistema para una veterinaria")
+    p=Proyecto("proyecto1","sistema para una veterinaria",1000)
     MgrProject().guardar(p)
-    p=Proyecto("proyecto2","sistema para una guarderia")
+    p=Proyecto("proyecto2","sistema para una guarderia",2000)
     MgrProject().guardar(p)
-    p=Proyecto("proyecto3","sistema para un consultorio")
+    p=Proyecto("proyecto3","sistema para un consultorio",3000)
     MgrProject().guardar(p)
-    p=Proyecto("proyecto4","sistema para un supermercado")
+    p=Proyecto("proyecto4","sistema para un supermercado",4000)
     MgrProject().guardar(p)
-    p=Proyecto("proyecto5","sistema para un banco")
+    p=Proyecto("proyecto5","sistema para un banco",5000)
     MgrProject().guardar(p)
     #MgrProject().borrar("proyecto5")
     #MgrProject().modificar("proyecto5","proyecto5","sistema para un banco2","Pendiente")
 
-@manager.command
-def createFase():
-    """ crea proyectos por default """
-    from models import Fase
-    from ctrl.mgrFase import MgrFase
-    f=Fase("fase1-proyecto1","fase inicial",1)
-    MgrFase().guardar(f)
-    f=Fase("fase2-proyecto1","fase",2)
-    MgrFase().guardar(f)
-    f=Fase("fase1-proyecto2","fase inicial",1)
-    MgrFase().guardar(f)
-    f=Fase("fase2-proyecto2","fase",2)
-    MgrFase().guardar(f)
-    f=Fase("fase1-proyecto3","fase inicial",1)
-    MgrFase().guardar(f)
-    f=Fase("fase2-proyecto3","fase",2)
-    MgrFase().guardar(f)
-    #MgrFase().borrar("fase2-proyecto3")
-    #MgrFase().modificar("fase1-proyecto3", "fase1-proyecto3","inicio",1)
 
 @manager.command
 def createTipoDeAtrib():
